da81b7ad48e99e6a3e2f45c4a2f1718773c19e68
Murat Altun <mraltun@gmail.com>
2022-10-24 17:59:54 +0300

    Added xxxBar components. Added Layout and ProtectedRoutes in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import LeftBar from "./components/leftBar/LeftBar";
+import Navbar from "./components/navbar/Navbar";
+import RightBar from "./components/rightBar/RightBar";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 const App = () => {
+  const currentUser = false;
+
   const Layout = () => {
-    return <div></div>;
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to='/login' />;
+    }
+
+    return children;
   };
 
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/profile/:id", element: <Profile /> },
+      ],
+    },
     {
       path: "/login",
       element: <Login />,
