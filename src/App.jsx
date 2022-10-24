b4072d9ec8e1c376c4a9bb81f41a0fba0f5bd566
Murat Altun <mraltun@gmail.com>
2022-10-24 17:33:11 +0300

    Register and it's scss is done. Added RRD

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 const App = () => {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 };
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,32 @@ import React from "react";
 import "./register.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className='register'>
+      <div className='card'>
+        <div className='left'>
+          <h1>Register</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            mollitia iste aliquam reprehenderit ut sunt maxime, doloribus error
+            velit fugiat!
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className='right'>
+          <h1>Register</h1>
+          <form>
+            <input type='text' placeholder='Username' />
+            <input type='email' placeholder='Email' />
+            <input type='password' placeholder='Password' />
+            <input type='text' placeholder='Name' />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
