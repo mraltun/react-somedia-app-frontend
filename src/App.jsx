6564d635107d10ac68959082ad05d7d7f9fcd1e5
Murat Altun <mraltun@gmail.com>
2022-10-25 13:18:46 +0300

    LeftBar scss is done. Added flex-grow to bars and main app

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,9 @@ const App = () => {
         <Navbar />
         <div style={{ display: "flex" }}>
           <LeftBar />
-          <Outlet />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
           <RightBar />
         </div>
       </div>
--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -20,10 +20,10 @@ const LeftBar = () => {
       <div className='container'>
         <div className='menu'>
           <div className='user'>
-            {/* <img
+            <img
               src='https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600'
               alt=''
-            /> */}
+            />
             <span>John Doe</span>
           </div>
           <div className='item'>
