71ccf3d3d961790943f6ac5474a55b0c112873a8
Murat Altun <mraltun@gmail.com>
2022-10-25 18:39:43 +0300

    Added Stories and linked inside Home

---
+++ src/components/stories/Stories.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import "./stories.scss";
+import { AuthContext } from "../../context/authContext";
+
+const Stories = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  //TEMPORARY DUMMY DATA
+  const stories = [
+    {
+      id: 1,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 3,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 4,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+
+  return (
+    <div className='stories'>
+      {stories.map((story) => (
+        <div className='story'>
+          <img src={story.img} alt='' />
+          <span>{story.name}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Stories;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,45 +1,13 @@
 import React from "react";
+import Posts from "../../components/posts/Posts";
+import Stories from "../../components/stories/Stories";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className='home'>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
-      <h1>STICKY_TEST</h1>
+      <Stories />
+      <Posts />
     </div>
   );
 };
