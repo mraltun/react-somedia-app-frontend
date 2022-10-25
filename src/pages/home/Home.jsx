480d3bcc446cc6a81bb9db779d7039d1e0d0efea
Murat Altun <mraltun@gmail.com>
2022-10-25 13:35:21 +0300

    Added CSS Sticky to bars

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,46 @@ import React from "react";
 import "./home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='home'>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+      <h1>STICKY_TEST</h1>
+    </div>
+  );
 };
 
 export default Home;
