0cdf438030840e345613a78481d91476a6804844
Murat Altun <mraltun@gmail.com>
2022-10-26 12:16:15 +0300

    Post jsx is done

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import "./post.scss";
 
-const Post = () => {
-  return <div className='post'>Post</div>;
+const Post = ({ post }) => {
+  return (
+    <div className='post'>
+      <div className='container'>
+        <div className='user'>
+          <div className='userInfo'>
+            <img src={post.img} alt='' />
+            <div className='details'>
+              <Link
+                to={`/profile/${post.userId}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <span>{post.name}</span>
+                <span className='date'>1 min ago</span>
+              </Link>
+            </div>
+          </div>
+          <MoreHorizIcon />
+        </div>
+        <div className='content'></div>
+        <div className='info'></div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
